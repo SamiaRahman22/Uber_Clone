7c390b3f0e07c0ad33b5e8724e8a53ed4e4eebb8
Samia Rahman <samiarahman7448@gmail.com>
2025-05-06 14:07:12 +0600

    pages frontend & Home page design done

---
+++ frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserLogin = () => {
+  return (
+    <div>
+      <form>
+
+      <h3>What's your email</h3>
+      <input required type="email"/>
+      </form>
+    </div>
+  )
+}
+
+export default UserLogin
